d588af3bdeec538312f19f1ec1999facb3ecb44a
RasoliDavide <rasoli.davide@iisgalvanimi.edu.it>
2019-10-28 11:18:57 +0100

    Sistemata struttura cartelle

---
+++ 1.2/client-angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client-angular';
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('http://localhost:3000/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
